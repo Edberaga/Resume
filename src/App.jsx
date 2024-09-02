a8fed767829e2415c8f034910c068b4e0ffda54b
Edberaga <72023119+Edberaga@users.noreply.github.com>
2024-09-02 15:07:56 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <main>
+      <h1>This guy is too lazy to make a resume at Web</h1>
+      <a href="/Resume.pdf" download>
+        <button>Donwload CV</button>
+      </a>
+    </main>
+  );
+}
