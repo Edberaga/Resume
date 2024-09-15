58a161b044569c2ccbd2264bf1384c3f39a51521
edberaga <edbetjonnathan@gmail.com>
2024-09-15 23:10:16 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <main>
-      <h1>This guy is too lazy to make a resume at Web</h1>
+      <h1>This guy is too lazy to make a resume at Web Test</h1>
       <a href="/Resume.pdf" download>
         <button>Donwload CV</button>
       </a>
